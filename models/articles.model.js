e646d51b0858beabf9b5fd71da6852c25f5b3ccb
HS <hs@HSs-MacBook-Pro.local>
2022-09-06 21:45:03 +0100

    ncnews ticket 4

---
+++ models/articles.model.js
@@ -0,0 +1,9 @@
+const db = require("../db/connection");
+
+exports.selectArticleById = (articleId) => {
+  return db
+    .query("SELECT * FROM articles WHERE article_id = $1;", [articleId])
+    .then((article) => {
+      return article.rows[0];
+    });
+};
